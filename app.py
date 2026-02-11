254dca2fd9b38aa8cac93673369c811ab4a4df74
Ayden Tange <au598764321@gmail.com>
2026-02-11 14:28:09 +0100

    Ajout des templates HTML et du dossier static

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Bienvenue sur ma page d'accueil !"
+    return render_template("home.html")
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    return "À propos de moi : Je suis en train d'apprendre à développer des applications web avec Flask !"
+    return render_template("about.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)

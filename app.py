7e876d89d279301dd1fc5d0eb3552676636f05ca
Ayden Tange <au598764321@gmail.com>
2026-02-11 14:47:50 +0100

    Ajout page localisation avec carte

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.route("/localisation")
+def localisation():
+    return render_template("localisation.html")
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
